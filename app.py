c72fa06d0bf9398bf4cdb5681ddbf04d09325937
Patrycja <patrycjatyra@protonmail.com>
2020-03-22 11:58:26 +0000

    renamed venv without a dot

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 import os
 from os import path
 if path.exists("env.py"):
-  import env 
+  import .venv 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 MONGO_DBNAME = os.environ.get('MONGO_DBNAME')
 MONGO_URI = os.environ.get('MONGO_URI')
