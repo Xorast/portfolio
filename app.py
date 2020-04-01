cef3295cd6c3c41bee08431dcbd8c7d1ff7cfca1
Patrycja <patrycjatyra@protonmail.com>
2020-04-01 15:23:07 +0100

    removed blank line

--- app.py
+++ app.py
@@ -11,7 +11,9 @@ MONGO_URI = os.environ.get('MONGO_URI')
 
 
 if __name__ == '__main__':
-
+  app.run(host=os.environ.get('HOSTNAME'),
+            port=int(os.environ.get('PORT')),
+            debug=os.environ.get('DEV'))
   app = Flask(__name__)
 
 @app.route('/')
